63f79dd2bac50d04a9ed13a91fa910fd08e5da76
Sathwik MD <sathwikmd2006@gmail.com>
2025-05-15 15:33:26 +0530

    to print 1 or 0 based on the user input conditions

---
+++ 4.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+int main() {
+    char name = 'A';
+    if(name=='A' || name=='c')
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("0");
+    }
+    return 0;
+}
+
